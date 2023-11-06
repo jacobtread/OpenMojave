dbab90ced10763ea6b16c2f765ab75d9fafe947b
Jacobtread <jacobtread@gmail.com>
2023-11-06 19:26:09 +1300

    LGTM parsing

--- game/src/esp/record/records/land.rs
+++ game/src/esp/record/records/land.rs
@@ -1,2 +1,15 @@
+use super::prelude::*;
+
+/// Landscape
 #[derive(Debug)]
-pub struct LAND {}
+pub struct LAND {
+    // TODO:
+}
+
+impl Record for LAND {
+    const TYPE: RecordType = RecordType::new(b"LAND");
+
+    fn parse<'b>(parser: &mut RecordParser<'_, 'b>) -> Result<Self, RecordParseError<'b>> {
+        todo!()
+    }
+}
