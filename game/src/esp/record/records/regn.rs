fca4acfd2625d32612f03a8f66119ae54603d4c6
Jacobtread <jacobtread@gmail.com>
2023-11-06 20:23:41 +1300

    REPU parsing

--- game/src/esp/record/records/regn.rs
+++ game/src/esp/record/records/regn.rs
@@ -1,2 +1,13 @@
+use super::prelude::*;
+
+/// Region
 #[derive(Debug)]
 pub struct REGN {}
+
+impl Record for REGN {
+    const TYPE: RecordType = RecordType::new(b"REGN");
+
+    fn parse<'b>(parser: &mut RecordParser<'_, 'b>) -> Result<Self, RecordParseError<'b>> {
+        todo!()
+    }
+}
