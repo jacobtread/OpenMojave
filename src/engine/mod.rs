b021d40e9c6425f6278e910487b5b85289378f37
Jacobtread <jacobtread@gmail.com>
2023-11-10 18:55:08 +1300

    Switched back to bevy

--- src/engine/mod.rs
+++
@@ -1,8 +0,0 @@
-use winit::window::Window;
-
-use self::renderer::RenderContext;
-
-pub mod components;
-pub mod events;
-pub mod renderer;
-pub mod systems;
--- src/engine/systems/init.rs
+++
@@ -1,5 +0,0 @@
-//! Systems for initializing the app
-
-use bevy_ecs::prelude::*;
-
-pub fn init(world: &mut World) {}
